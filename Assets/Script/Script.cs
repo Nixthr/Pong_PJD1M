3d95f261e53c3cde607f4736b1b115b9332be4be
Nixthr <rodriguesraul746@gmail.com>
2026-09-17 11:39:58 -0300

    script

---
+++ Assets/Script/Script.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+  
+    float velocityPaddelRed = 2.5f;
+    float velocityBall = 3.5f;
+    int point = 0;
+    string swapball;
+    bool ballInMiddle = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float velocityPaddelBlue = 2.5f;
+        int ballQuantity = 1;
+        string paddelRed;
+        bool paddelBlueInUp = false;
+        bool paddelRedInDown = true;
+        bool masterKey = true;
+        bool heroIsLevel5 = false;
+        bool passLevel;
+
+     
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
